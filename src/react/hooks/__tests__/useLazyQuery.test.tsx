a7b5d7203bc172600cc60b37eda14b93f34cd2e6
Jerel Miller <jerelmiller@gmail.com>
2025-02-11 12:11:21 -0700

    Handle changing queries

--- src/react/hooks/__tests__/useLazyQuery.test.tsx
+++ src/react/hooks/__tests__/useLazyQuery.test.tsx
@@ -536,7 +536,7 @@ describe("useLazyQuery Hook", () => {
     }
   });
 
-  it.skip("changing queries", async () => {
+  it("changing queries", async () => {
     const query1 = gql`
       query {
         hello
@@ -577,7 +577,6 @@ describe("useLazyQuery Hook", () => {
 
       expect(result).toEqualLazyQueryResult({
         data: undefined,
-        error: undefined,
         called: false,
         loading: false,
         networkStatus: NetworkStatus.ready,
